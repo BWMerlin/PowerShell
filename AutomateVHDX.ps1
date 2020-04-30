e5ffcb6ffe54fc35d89c45ac828c1cc5ccab8d99
BW~Merlin <857916+BWMerlin@users.noreply.github.com>
2020-04-30 21:19:23 +1000

    Got the xml import, modify and save working.

--- AutomateVHDX.ps1
+++ AutomateVHDX.ps1
@@ -11,8 +11,17 @@
 #what name they want the Hyper-V guest to be called
 #What name they want the guest OS to be called
 
-
+$ISOPath = "C:\Users\Administrator\Downloads\SW_DVD9_Win_Pro_10_1909_64BIT_English_Pro_Ent_EDU_N_MLF_X22-17395.ISO"
+#We need to know where the ISO we will be using it located so we will ask the user to give us the FULL path to the iso.
 $ISOPath = Read-Host "Please type in the FULL path to the ISO that will be used"
+#To get the editions available to be installed we need to mount the image first
+$drivemount = Mount-DiskImage -ImagePath $ISOPath
+#Now we have the ISO mounted we need to get the drive letter of the volume
+$drive = Get-DiskImage -DevicePath $drivemount.DevicePath | Get-Volume
+#Now we have the drive letter when need to get the list of indexs which contains the description of what editions are aviable to be installed
+dism /get-wiminfo /wimfile:$($drive.DriveLetter):\sources\install.wim | Write-Host
+#Now we can unount the drive
+dismount-diskimage -imagepath $isopath
 $WindowsEdition = Read-Host "Please type the Windows Edition you wish to install"
 #"$env:userprofile\downloads\SW_DVD9_Win_Pro_10_1909_64BIT_English_Pro_Ent_EDU_N_MLF_X22-17395.ISO"
 $VMName = Read-Host "Please enter a name for the virtual machine.  This will be used in both the Hyper-V console and the guest OS host name"
@@ -27,8 +36,14 @@ $Downloadlocation = Read-Host "Please type in the location you would like to dow
 #We want to download Convert-WindowsImage from https://github.com/x0nn/Convert-WindowsImage/blob/master/Convert-WindowsImage.ps1 so that we can make a VHDX with an unattend from an ISO
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/x0nn/Convert-WindowsImage/master/Convert-WindowsImage.ps1" -OutFile $Downloadlocation\Convert-WindowsImage.ps1
 
+#Let's download the unattend.xml file from
+
+
 #We want to edit out unattend.xml, first we import it
-Get-Content $Unattend
+$theunattend = [xml](Get-Content $unattendlocation)
+
+$theunattend.unattend.settings.component.setupuilanguage[0].UILanguage="en-AU"
+$theunattend.save("c:\myfile.xml")
 
 
 #Next we create our VM, first we dotsource the script
@@ -40,7 +55,7 @@ Convert-WindowsImage -SourcePath $ISOPath -VHDFormat VHDX -Edition $WindowsEditi
 #Script to install Hyper-V service, Install guest OS of Window 10, install OBS and VS Studio onto the guest OS
 
 #We will make the "assumption" that Hyper-V is not installed and use ifnot instead of if
-if (-not ((Get-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Hyper-V-All').State -eq 'Enabled')
+if (-not ((Get-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Hyper-V-All').State -eq 'Enabled'))
     {Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-All" -NoRestart}
 
 #Check if Hyper-V Virtual Machine guest with the name of OBS exists
