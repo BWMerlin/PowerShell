76ab74d2e37c872f64b1a8d5a7cb89ef2a026d28
BWMerlin <857916+BWMerlin@users.noreply.github.com>
2021-12-31 21:51:41 +1000

    Better layed out the labels

--- PS WS1.ps1
+++ PS WS1.ps1
@@ -76,7 +76,7 @@ $main_form.Controls.Add($Label)
 
 #A combo box which will contain a list of users from AD
 $ComboBox = New-Object System.Windows.Forms.ComboBox
-$ComboBox.Width = 300
+$ComboBox.Width = 200
 #We want to search the our domain but only in the specified OU but we want to search by "sID" number so we can barcode scan them in
 $Users = Get-ADUser -filter * -Properties extensionAttribute6 -SearchBase $OU -Server $DC
 
@@ -85,7 +85,7 @@ Foreach ($User in $Users)
 #$ComboBox.Items.Add($User.SamAccountName);
 $ComboBox.Items.Add($User.extensionAttribute6);
 }
-$ComboBox.Location  = New-Object System.Drawing.Point(60,10)
+$ComboBox.Location  = New-Object System.Drawing.Point(200,10)
 $main_form.Controls.Add($ComboBox)
 
 #A label that states "Users AD login name"
@@ -98,58 +98,72 @@ $main_form.Controls.Add($Label2)
 #The resuts that actuall show the Users AD login name
 $Label3 = New-Object System.Windows.Forms.Label
 $Label3.Text = ""
-$Label3.Location  = New-Object System.Drawing.Point(110,40)
+$Label3.Location  = New-Object System.Drawing.Point(200,40)
 $Label3.AutoSize = $true
 $main_form.Controls.Add($Label3)
 
 #A label that states "Users device in Workspace ONE:
 $Label4 = New-Object System.Windows.Forms.Label
 $Label4.Text = "Users device in Workspace ONE"
-$Label4.Location  = New-Object System.Drawing.Point(0,80)
+$Label4.Location  = New-Object System.Drawing.Point(0,70)
 $Label4.AutoSize = $true
 $main_form.Controls.Add($Label4)
 
 #The results returned showing the users device from Workspace ONE
 $Label5 = New-Object System.Windows.Forms.Label
 $Label5.Text = ""
-$Label5.Location  = New-Object System.Drawing.Point(110,80)
+$Label5.Location  = New-Object System.Drawing.Point(200,70)
 $Label5.AutoSize = $true
 $main_form.Controls.Add($Label5)
 
 #A label that states "The users Workspace ONE ID"
 $Label6 = New-Object System.Windows.Forms.Label
 $Label6.Text = "The users Workspace ONE ID"
-$Label6.Location  = New-Object System.Drawing.Point(110,80)
+$Label6.Location  = New-Object System.Drawing.Point(0,100)
 $Label6.AutoSize = $true
 $main_form.Controls.Add($Label6)
 
 #The actual results for the "Workspace ONE ID of the user"
 $Label7 = New-Object System.Windows.Forms.Label
 $Label7.Text = ""
-$Label7.Location  = New-Object System.Drawing.Point(110,80)
+$Label7.Location  = New-Object System.Drawing.Point(200,100)
 $Label7.AutoSize = $true
 $main_form.Controls.Add($Label7)
+
+#A label that says "Scan device serial number"
+$Label8 = New-Object System.Windows.Forms.Label
+$Label8.Text = "Scan device serial number"
+$Label8.Location  = New-Object System.Drawing.Point(0,130)
+$Label8.AutoSize = $true
+$main_form.Controls.Add($Label8)
 
 #Let's have a button that people can push #PushItPushItRealGood
 $Button = New-Object System.Windows.Forms.Button
 $Button.Location = New-Object System.Drawing.Size(400,10)
-$Button.Size = New-Object System.Drawing.Size(120,23)
-$Button.Text = "Check"
+$Button.Size = New-Object System.Drawing.Size(150,23)
+$Button.Text = "Check Active Directory"
 $main_form.Controls.Add($Button)
 
 #We are going to make a second button to check Workspace ONE but really we don't need one as we could use the first but let's try
 $Button2 = New-Object System.Windows.Forms.Button
-$Button2.Location = New-Object System.Drawing.Size(400,80)
-$Button2.Size = New-Object System.Drawing.Size(120,23)
-$Button2.Text = "Check Workspace ONE"
+$Button2.Location = New-Object System.Drawing.Size(400,70)
+$Button2.Size = New-Object System.Drawing.Size(150,23)
+$Button2.Text = "Check users device in Workspace ONE"
 $main_form.Controls.Add($Button2)
 
-#Now let's give users a button that they can click on to assing a device to a user in Workspace ONE
+#We want to make a button to allow us to check what the users Workspace ONE id number is
 $Button3 = New-Object System.Windows.Forms.Button
-$Button3.Location = New-Object System.Drawing.Size(400,120)
-$Button3.Size = New-Object System.Drawing.Size(120,23)
-$Button3.Text = "Assign device to user in Workspace ONE"
+$Button3.Location = New-Object System.Drawing.Size(400,100)
+$Button3.Size = New-Object System.Drawing.Size(150,23)
+$Button3.Text = "Check users Workspace ONE ID"
 $main_form.Controls.Add($Button3)
+
+#Now let's give users a button that they can click on to assing a device to a user in Workspace ONE
+$Button4 = New-Object System.Windows.Forms.Button
+$Button4.Location = New-Object System.Drawing.Size(400,130)
+$Button4.Size = New-Object System.Drawing.Size(150,23)
+$Button4.Text = "Assign device to user"
+$main_form.Controls.Add($Button4)
 
 #What code do we want to actually run when people "Salt and Peper" our button
 $Button.Add_Click(
@@ -166,6 +180,15 @@ $Button2.Add_Click(
 $usertocheck = $Label3.Text 
 $device = Invoke-RestMethod -Uri "$server/api/mdm/devices/search?user=$usertocheck" -Method Get -Headers $header_v1
 $Label5.Text = $device.Devices.SerialNumber
+}
+)
+
+#Let us check Workspace ONE for the users ID number
+$Button3.Add_Click(
+{
+$usertocheck = $Label3.Text 
+$UserIDCheck = Invoke-RestMethod -Uri "$server/API/system/users/search?username=$usertocheck" -Method Get -Headers $header_v1
+$Label7.Text = $UserIDCheck.Users.id.Value
 }
 )
 
@@ -200,7 +223,7 @@ $DeviceBody = @{
 #Assign that actual device to a user in Workspace ONE
 
 <# This code may have changed
-$Button3.Add_Click(
+$Button4.Add_Click(
 {
 $usertocheck = $Label3.Text 
 $deviceenrollment = Invoke-RestMethod -Uri "$server/API/system/users/$userid/registerdevice" -Method Post -Headers $header_v1 -Body ($body | ConvertTo-Json)
@@ -209,10 +232,10 @@ $deviceenrollment = Invoke-RestMethod -Uri "$server/API/system/users/$userid/reg
 #>
 
 #Ths is possible the new correct code
-$Button3.Add_Click(
+$Button4.Add_Click(
 {
 $usertocheck = $Label3.Text 
-$deviceenrollment = Invoke-RestMethod -Uri "$server/API/system/users/$userid/registerdevice" -Method Post -Headers $header_v1 -Body $DeviceBody
+$DeviceEnrollment = Invoke-RestMethod -Uri "$server/API/system/users/$userid/registerdevice" -Method Post -Headers $header_v1 -Body $DeviceBody
 }
 )
 
